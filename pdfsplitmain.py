5550b6c1e440590156804a2cca224117d6dbe856
ockhamlabs <hello@ockhamlabs.ai>
2024-02-28 20:52:49 -0500

    Update pdfsplitmain.py

--- pdfsplitmain.py
+++ pdfsplitmain.py
@@ -1,35 +1,43 @@
 import streamlit as st
 import PyPDF2
 import os
-from tempfile import mkdtemp
+from tempfile import mkdtemp, NamedTemporaryFile
 import shutil
+import zipfile
 
 def split_pdf_into_pages(pdf_file, output_folder):
-    reader = PyPDF2.PdfFileReader(pdf_file)
-    for page_num in range(reader.numPages):
-        writer = PyPDF2.PdfFileWriter()
-        writer.addPage(reader.getPage(page_num))
-        output_filename = f"{output_folder}/{page_num+1:03d}.pdf"
+    reader = PyPDF2.PdfReader(pdf_file)
+    for page_num in range(len(reader.pages)):
+        writer = PyPDF2.PdfWriter()
+        writer.add_page(reader.pages[page_num])
+        output_filename = f"{output_folder}/Page_{page_num+1:03d}.pdf"
         with open(output_filename, 'wb') as output_file:
             writer.write(output_file)
 
-def create_zip(folder_path, zip_name):
-    shutil.make_archive(zip_name, 'zip', folder_path)
-    shutil.rmtree(folder_path)  # Clean up folder
+def zip_files(directory):
+    zip_filename = os.path.join(directory, "split_pages.zip")
+    with zipfile.ZipFile(zip_filename,'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file != "split_pages.zip":  # Avoid including the zip file itself
+                    zipf.write(os.path.join(root, file), file)
+    return zip_filename
 
 st.title('PDF Splitter App')
 
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
 if uploaded_file is not None:
-    with st.spinner('Splitting PDF...'):
-        temp_dir = mkdtemp()
-        split_pdf_into_pages(uploaded_file, temp_dir)
-        zip_name = "split_pages"
-        create_zip(temp_dir, zip_name)
-        with open(f"{zip_name}.zip", "rb") as f:
-            btn = st.download_button(
-                label="Download ZIP",
-                data=f,
-                file_name=f"{zip_name}.zip",
-                mime="application/zip"
-            )
+    temp_dir = mkdtemp()
+    split_pdf_into_pages(uploaded_file, temp_dir)
+    zip_path = zip_files(temp_dir)
+
+    with open(zip_path, "rb") as f:
+        st.download_button(
+            label="Download Split Pages as ZIP",
+            data=f,
+            file_name="split_pages.zip",
+            mime="application/zip"
+        )
+
+    # Cleanup
+    shutil.rmtree(temp_dir)  # Remove temporary directory and files after downloading
